3cddcc46a4b27ba4da7bf4d476100030eac0aa4b
linxiaofeifz <1302665507@qq.com>
2014-11-23 23:55:36 +0800

    邮件工具类

---
+++ src/main/java/com/bitcoin/utils/MailUtils.java
@@ -0,0 +1,96 @@
+package com.bitcoin.utils;
+
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.Multipart;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+/**
+ * MailUtils
+ * 
+ * @author Lin
+ * 
+ */
+public class MailUtils {
+
+	private String smtpHost;
+	private String username;
+	private String password;
+
+	/**
+	 * 发送邮件
+	 * 
+	 * @param toMail
+	 * @param subject
+	 * @param text
+	 */
+	public void sendMailUtis(String toMail, String subject, String text) {
+		Properties properties = new Properties();
+		properties.put("mail.smtp.host", smtpHost);
+		properties.put("mail.smtp.socketFactory.port", "465");
+		properties.put("mail.smtp.socketFactory.class",
+				"javax.net.ssl.SSLSocketFactory");
+
+		properties.put("mail.smtp.auth", "true");
+		properties.put("mail.smtp.port", "465");
+
+		Session session = Session.getDefaultInstance(properties,
+				new Authenticator() {
+					protected javax.mail.PasswordAuthentication getPasswordAuthentication() {
+						return new javax.mail.PasswordAuthentication(username,
+								password);
+					}
+				});
+		MimeMessage mimeMessage = new MimeMessage(session);
+		try {
+			InternetAddress address = new InternetAddress(username);
+			mimeMessage.setFrom(address);
+			mimeMessage.addRecipient(Message.RecipientType.TO,
+					new InternetAddress(toMail));
+			mimeMessage.setSubject(subject);
+			BodyPart mdp = new MimeBodyPart();
+			mdp.setContent(text, "text/html;charset=gb2312");
+			Multipart mm = new MimeMultipart();
+			mm.addBodyPart(mdp);
+			mimeMessage.setContent(mm);
+			mimeMessage.saveChanges();
+			// mimeMessage.setText(text);
+			Transport.send(mimeMessage);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+	public String getSmtpHost() {
+		return smtpHost;
+	}
+
+	public void setSmtpHost(String smtpHost) {
+		this.smtpHost = smtpHost;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
