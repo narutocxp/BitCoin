9456bf756e02c49a591a36be19abc7dc77b0d15f
saplin <405548739@qq.com>
2014-11-25 21:52:06 +0800

    loginaction

---
+++ src/main/java/com/bitcoin/action/LoginAction.java
@@ -0,0 +1,56 @@
+package com.bitcoin.action;
+
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Namespace;
+import org.apache.struts2.convention.annotation.ParentPackage;
+import org.apache.struts2.convention.annotation.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+import com.bitcoin.service.RegisterService;
+import com.opensymphony.xwork2.ActionSupport;
+
+
+@Controller
+@Scope("prototype")
+@ParentPackage("struts-default")
+@Namespace(value="/*")
+public class LoginAction  extends ActionSupport {
+	
+	private String userMail;
+	private String userPassword;
+
+	@Autowired
+	private LoginService loginService;
+
+	public String getUserMail() {
+		return userMail;
+	}
+
+	public void setUserMail(String userMail) {
+		this.userMail = userMail;
+	}
+
+	public String getUserPassword() {
+		return userPassword;
+	}
+
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+
+
+	@Action(value="login",results={@Result(name="success",location="/page/loginSuccess.jsp"),@Result(name="error",location="/page/loginFail.jsp")})
+	public String login() throws Exception{
+		
+		 if(loginService.isloginSuccess(userMail,userPassword))
+			 return SUCCESS;
+		 
+		 return ERROR;
+		
+		
+	}
+	
+	
+}
